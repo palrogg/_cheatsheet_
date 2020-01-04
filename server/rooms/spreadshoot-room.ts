04e8975e5f13bc820b48f7045141edd961b7af36
Paul Ronga <paul.ronga@laposte.net>
2020-01-04 10:36:40 +0100

    clean server-side movements

--- server/rooms/spreadshoot-room.ts
+++ server/rooms/spreadshoot-room.ts
@@ -1,6 +1,16 @@
 import { Room } from "colyseus";
 import { Schema, MapSchema, type } from "@colyseus/schema";
 
+const bounds = {
+  // min, max, range
+  'x': [0, 4, 5],
+  'y': [0, 8, 9]
+};
+
+const MIN_ROW_NUMBER = 1; // 0 = ligne du header
+const MAX_ROW_NUMBER = 8;
+const TABLE_WIDTH = 5;
+
 // Reference: https://docs.colyseus.io/state/schema/
 class Entity extends Schema {
   @type("number")
@@ -44,27 +54,39 @@ export class SpreadshootRoom extends Room {
     onLeave (client) {
         this.broadcast(`${ client.sessionId } left.`);
     }
-
+    
+    getNewPosition (entity, axis, factor, steps = 1) {
+      let newPos = entity[axis] + (factor * steps);
+      
+      // under limit?
+      if(newPos < bounds[axis][0]){
+        newPos += bounds[axis][2];
+        
+      // exceeds limit?
+      }else if(newPos > bounds[axis][1]){
+        newPos -= bounds[axis][2];
+      }
+      console.log('New position:', newPos);
+      return newPos;
+    }
+    
     // TODO: gestion deplacements
     onMessage (client, data) {
         console.log("Message from", client.sessionId, ":", data);
         if('keycode' in data){
           if(data['keycode'] == 37){
-            console.log('left');
-            // temp! we'll manage mvmts later
-            this.state.entities[client.sessionId].x -= 1;
+            // left
+            this.state.entities[client.sessionId].x = this.getNewPosition(this.state.entities[client.sessionId], 'x', -1);;
           }else if(data['keycode'] == 38){
-            console.log('up')
-            // temp! we'll manage mvmts later
-            this.state.entities[client.sessionId].y -= 1;
+            // up
+            this.state.entities[client.sessionId].y = this.getNewPosition(this.state.entities[client.sessionId], 'y', -1);
           }else if(data['keycode'] == 39){
             console.log('right')
-            // temp! we'll manage mvmts later
-            this.state.entities[client.sessionId].x += 1;
+            // right
+            this.state.entities[client.sessionId].x = this.getNewPosition(this.state.entities[client.sessionId], 'x', 1);
           }else if(data['keycode'] == 40){
-            console.log('down')
-            // temp! we'll manage mvmts later
-            this.state.entities[client.sessionId].y += 1;
+            // down
+            this.state.entities[client.sessionId].y = this.getNewPosition(this.state.entities[client.sessionId], 'y', 1);
           }
           console.log('Player position is now', this.state.entities[client.sessionId].x, ',', this.state.entities[client.sessionId].y)
         }
