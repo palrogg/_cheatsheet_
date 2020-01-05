c59c142542b74e2ce87c2d52b39e0ff1ed77fb88
Paul Ronga <paul.ronga@laposte.net>
2020-01-05 16:38:51 +0100

    Backend -> XHR

--- client/src/_helpers/i18n.js
+++ client/src/_helpers/i18n.js
@@ -9,16 +9,17 @@
 */
 
 import i18n from 'i18next';
-import Backend from 'i18next-xhr-backend';
+import XHR from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 i18n
-  .use(Backend)
+  .use(XHR)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     //lng: 'en', // overrides detection
+    
     fallbackLng: 'en', // preferred language
     debug: false,
     detection: {
